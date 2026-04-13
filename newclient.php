887468750c69892933ea66a4d8f372c818b1f26f
strc12 <ric@oundleschool.org.uk>
2026-04-13 13:51:18 +0100

    test

--- newclient.php
+++ newclient.php
@@ -7,7 +7,7 @@ require_once 'auth.php';
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Create Client</title>
-<!-- Bootstrap CSS -->
+<!-- Bootstrap CSS ff-->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
